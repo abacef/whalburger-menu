75546418df727d88e8a1da71140edba7b6341497
bacef <bacef@pop-os.localdomain>
2022-10-17 00:56:25 -0700

    Got modal to work

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Vinilla from "./lib/Vinilla.svelte";
+import Whalburger from "./lib/Whalburger.svelte";
+
+
+</script>
+
+<h2>
+  ModalExample
+</h2>
+
+<!-- <Whalburger/> -->
+<Vinilla/>
---
+++ src/lib/Whalburger.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  let open = false
+
+  function changeWhalburger() {
+    open = !open
+  }
+</script>
+
+
+<table>
+
+  <tr>
+    <td>
+      <button on:click={changeWhalburger}>
+        ☰
+      </button>
+    </td>
+  </tr>
+
+  
+  <!-- {#if open}
+    <tr>
+      one option
+    </tr>
+    <tr>
+      2 opt
+    </tr>
+  {/if} -->
+
+</table>
